7266df9d6a276c2cf2c817ae254d0ecc7cf9e7cd
weijiasu <weijiasu0804@gmail.com>
2023-04-17 12:12:29 -0400

    kraken

--- Kraken/getfastq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bam_path="/data/zhanglab/Weijia_Su/WGS/GBM_tumor_022023/"
-
-name=$1
-samtools view -h -b -f 4 $bam_path$name".cs.rmdup.RG.bam" |samtools fastq > $name".upmapped.fastq";
